3727dbe7c5737503cdf4e2bdc1ca0211090fa872
raneshilpa <choudharishilpa83@gmail.com>
2022-02-26 14:13:44 -0500

    StringConcept1Classadded

---
+++ src/com/company/StringConcept1Class.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class StringConcept1Class {
+    public static void main(String[] args) {
+        String str = "Shilpa";
+        String str1 = "Rane";
+
+        System.out.println("sTRING IS "+str);
+        System.out.println("Surname"+str1);
+
+
+
+
+    }
+
+}
